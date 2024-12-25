ee0af2c5550d6d6c02a6113e03823ade77761dad
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-12-26 03:35:17 +0400

    initial project structure refactoring - merge all sources for future rewrite

---
+++ plugin/src/main/java/com/cmakeplugin/CMakeStructureViewElement.java
@@ -0,0 +1,181 @@
+package com.cmakeplugin;
+
+import com.cmakeplugin.utils.CMakeIFWHILEcheck;
+import com.cmakeplugin.utils.CMakePDC;
+import com.cmakeplugin.utils.CMakePSITreeSearch;
+import com.cmakeplugin.utils.CMakeVarStringUtil;
+import com.intellij.ide.projectView.PresentationData;
+import com.intellij.ide.structureView.StructureViewTreeElement;
+import com.intellij.ide.util.treeView.smartTree.SortableTreeElement;
+import com.intellij.ide.util.treeView.smartTree.TreeElement;
+import com.intellij.navigation.ItemPresentation;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.psi.NavigatablePsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.PlatformIcons;
+import java.util.stream.Stream;
+import javax.swing.Icon;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class CMakeStructureViewElement
+    implements StructureViewTreeElement, SortableTreeElement {
+  NavigatablePsiElement element;
+  ItemPresentation presentation;
+  String presentableText;
+  String notesText = null;
+  Icon icon = null;
+  TextAttributesKey attributesKey = null;
+
+  CMakeStructureViewElement(NavigatablePsiElement element) {
+    this.element = element;
+    this.presentation = element.getPresentation();
+    this.presentableText = element.getText();
+  }
+
+  @Override
+  public Object getValue() {
+    return element;
+  }
+
+  @Override
+  public void navigate(boolean requestFocus) {
+    element.navigate(requestFocus);
+  }
+
+  @Override
+  public boolean canNavigate() {
+    return element.canNavigate();
+  }
+
+  @Override
+  public boolean canNavigateToSource() {
+    return element.canNavigateToSource();
+  }
+
+  @NotNull
+  @Override
+  public String getAlphaSortKey() {
+    return presentableText != null ? presentableText : "";
+  }
+
+  @NotNull
+  @Override
+  public ItemPresentation getPresentation() {
+    return new PresentationData(presentableText, notesText, icon, attributesKey);
+  }
+
+  @Override
+  @NotNull
+  public TreeElement[] getChildren() {
+    return EMPTY_ARRAY;
+  }
+
+  boolean isVarDef(NavigatablePsiElement element) {
+    return CMakePSITreeSearch.existReferenceTo(element)
+        || (CMakeIFWHILEcheck.couldBeVarDef(element)
+        && CMakeVarStringUtil.isPredefinedCMakeVar(element.getText()));
+  }
+}
+
+class CMakeFileElement extends CMakeStructureViewElement {
+
+  CMakeFileElement(PsiFile element) {
+    super(element);
+  }
+
+  @NotNull
+  @Override
+  public ItemPresentation getPresentation() {
+    if (CMakePlusPDC.CMAKE_FILE_CLASS.isInstance(element) && presentation != null)
+      return presentation;
+    return super.getPresentation();
+  }
+
+  @Override
+  @NotNull
+  public TreeElement[] getChildren() {
+    if (!CMakePlusPDC.CMAKE_FILE_CLASS.isInstance(element)) return super.getChildren();
+
+    Stream<TreeElement> macroElements =
+        PsiTreeUtil.findChildrenOfAnyType(element, CMakePDC.MACRO_CLASS).stream()
+            .map(MacroElement::new);
+
+    Stream<TreeElement> functionElements =
+        PsiTreeUtil.findChildrenOfAnyType(element, CMakePDC.FUNCTION_CLASS).stream()
+            .map(FunctionElement::new);
+
+    Stream<TreeElement> varDefElements =
+        PsiTreeUtil.findChildrenOfAnyType(element, CMakePlusPDC.VARDEF_CLASS).stream()
+            .filter(this::isVarDef)
+            .filter(this::hasNoFunMacroParent)
+            .map(VarDefElement::new);
+
+    return Stream.of(functionElements, macroElements, varDefElements)
+        .flatMap(s -> s)
+        .toArray(TreeElement[]::new);
+  }
+
+  private boolean hasNoFunMacroParent(NavigatablePsiElement varDef) {
+    return PsiTreeUtil.getParentOfType(varDef, CMakePDC.FUNCTION_CLASS, CMakePDC.MACRO_CLASS)
+        == null;
+  }
+}
+
+abstract class FunMacroBase extends CMakeStructureViewElement {
+
+  FunMacroBase(NavigatablePsiElement element) {
+    super(element);
+    presentableText = CMakePSITreeSearch.getFunMacroName(element);
+    notesText = CMakePSITreeSearch.getFunMacroArgs(element);
+  }
+
+  @Override
+  public void navigate(boolean requestFocus) {
+    NavigatablePsiElement name = CMakePSITreeSearch.getFunMacroNameElement(element);
+    name = (name != null) ? name : element;
+    name.navigate(requestFocus);
+  }
+
+  @Override
+  @NotNull
+  public TreeElement[] getChildren() {
+    return PsiTreeUtil.findChildrenOfAnyType(element, CMakePlusPDC.VARDEF_CLASS).stream()
+        .filter(this::isVarDef)
+        .map(VarDefElement::new)
+        .toArray(TreeElement[]::new);
+  }
+}
+
+class MacroElement extends FunMacroBase {
+
+  MacroElement(NavigatablePsiElement element) {
+    super(element);
+    icon = CMakePlusPDC.ICON_CMAKE_MACRO;
+    attributesKey = CMakeSyntaxHighlighter.MACROS;
+  }
+}
+
+class FunctionElement extends FunMacroBase {
+
+  FunctionElement(NavigatablePsiElement element) {
+    super(element);
+    icon = CMakePlusPDC.ICON_CMAKE_FUNCTION;
+    attributesKey = CMakeSyntaxHighlighter.FUNCTION;
+  }
+}
+
+class VarDefElement extends CMakeStructureViewElement {
+
+  VarDefElement(NavigatablePsiElement element) {
+    super(element);
+    if (presentation != null
+        && (notesText = presentation.getPresentableText()) != null
+        && notesText.length() > 20) notesText = notesText.substring(20);
+    icon = PlatformIcons.VARIABLE_ICON;
+    attributesKey =
+        CMakeVarStringUtil.isPredefinedCMakeVar(element.getText())
+            ? CMakeSyntaxHighlighter.CMAKE_VAR_DEF
+            : CMakeSyntaxHighlighter.VAR_DEF;
+  }
+}
---
+++ plugin/src/main/java/com/cmakeplugin/CMakeStructureViewModel.java
@@ -0,0 +1,133 @@
+package com.cmakeplugin;
+
+import com.cmakeplugin.utils.CMakePDC;
+import com.cmakeplugin.utils.CMakePlusPDC2;
+import com.intellij.ide.structureView.*;
+import com.intellij.ide.util.treeView.smartTree.ActionPresentation;
+import com.intellij.ide.util.treeView.smartTree.ActionPresentationData;
+import com.intellij.ide.util.treeView.smartTree.Filter;
+import com.intellij.ide.util.treeView.smartTree.Sorter;
+import com.intellij.ide.util.treeView.smartTree.TreeElement;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.PlatformIcons;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class CMakeStructureViewModel extends StructureViewModelBase
+    implements StructureViewModel.ElementInfoProvider {
+
+  public CMakeStructureViewModel(PsiFile psiFile, @Nullable Editor editor) {
+    super(psiFile, editor, new CMakeFileElement(psiFile));
+  }
+
+  @NotNull
+  public Sorter[] getSorters() {
+    return new Sorter[] {Sorter.ALPHA_SORTER};
+  }
+
+  @Override
+  @NotNull
+  public Filter[] getFilters() {
+    return new Filter[] {new FunctionFilter(), new MacroFilter(), new VarDefFilter()};
+  }
+
+  @Override
+  @NotNull
+  protected Class[] getSuitableClasses() {
+    return new Class[] {
+      CMakePlusPDC.CMAKE_FILE_CLASS,
+      CMakePDC.FUNCTION_CLASS,
+      CMakePDC.MACRO_CLASS,
+      CMakePlusPDC.VARDEF_CLASS
+    };
+  }
+
+  @Override
+  public boolean isAlwaysShowsPlus(StructureViewTreeElement element) {
+    return false;
+  }
+
+  @Override
+  public boolean isAlwaysLeaf(StructureViewTreeElement element) {
+    return CMakePlusPDC.CMAKE_FILE_CLASS.isInstance(element);
+  }
+
+  private static class VarDefFilter implements Filter {
+
+    @Override
+    public boolean isVisible(TreeElement treeNode) {
+      return !(treeNode instanceof VarDefElement);
+    }
+
+    @Override
+    public boolean isReverted() {
+      return true;
+    }
+
+    @NotNull
+    @Override
+    public ActionPresentation getPresentation() {
+      return new ActionPresentationData(
+          "Show Variables (re)definitions", null, CMakePlusPDC.ICON_VAR);
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+      return "SHOW_VAR_DEF";
+    }
+  }
+
+  private static class MacroFilter implements Filter {
+
+    @Override
+    public boolean isVisible(TreeElement treeNode) {
+      return !(treeNode instanceof MacroElement);
+    }
+
+    @Override
+    public boolean isReverted() {
+      return true;
+    }
+
+    @NotNull
+    @Override
+    public ActionPresentation getPresentation() {
+      return new ActionPresentationData(
+          "Show Macros definitions", null, CMakePlusPDC.ICON_CMAKE_MACRO);
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+      return "SHOW_MACRO";
+    }
+  }
+
+  private static class FunctionFilter implements Filter {
+
+    @Override
+    public boolean isVisible(TreeElement treeNode) {
+      return !(treeNode instanceof FunctionElement);
+    }
+
+    @Override
+    public boolean isReverted() {
+      return true;
+    }
+
+    @NotNull
+    @Override
+    public ActionPresentation getPresentation() {
+      return new ActionPresentationData(
+          "Show Functions definitions", null, CMakePlusPDC.ICON_CMAKE_FUNCTION);
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+      return "SHOW_FUNCTIONS";
+    }
+  }
+}
